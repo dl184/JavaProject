74fc1cc049e7265582a4bd505e839146d63ff4e0
Steven Bonner <stevenbonner1990@gmail.com>
2018-07-12 16:34:45 +0100

    finshed dinosaur test

--- src/main/java/db/HibernateUtil.java
+++ src/main/java/db/HibernateUtil.java
@@ -1,27 +1,27 @@
-package db;
-import org.eclipse.jetty.websocket.common.SessionFactory;
-
-
-
-import javax.security.auth.login.Configuration;
-
-public class HibernateUtil {
-
-    private static final SessionFactory sessionFactory = buildSessionFactory();
-
-    private static SessionFactory buildSessionFactory() {
-        try {
-            // Create the SessionFactory from hibernate.cfg.xml
-            return new Configuration().configure().buildSessionFactory();
-        }
-        catch (Throwable ex) {
-            // Make sure you log the exception, as it might be swallowed
-            System.err.println("Initial SessionFactory creation failed." + ex);
-            throw new ExceptionInInitializerError(ex);
-        }
-    }
-
-    public static SessionFactory getSessionFactory() {
-        return sessionFactory;
-    }
-}
+//package db;
+//import org.eclipse.jetty.websocket.common.SessionFactory;
+//import org.hibernate.SessionFactory;
+//
+//
+//import javax.security.auth.login.Configuration;
+//
+//public class HibernateUtil {
+//
+//    private static final SessionFactory sessionFactory = buildSessionFactory();
+//
+//    private static SessionFactory buildSessionFactory() {
+//        try {
+//            // Create the SessionFactory from hibernate.cfg.xml
+//            return new Configuration().configure().buildSessionFactory();
+//        }
+//        catch (Throwable ex) {
+//            // Make sure you log the exception, as it might be swallowed
+//            System.err.println("Initial SessionFactory creation failed." + ex);
+//            throw new ExceptionInInitializerError(ex);
+//        }
+//    }
+//
+//    public static SessionFactory getSessionFactory() {
+//        return sessionFactory;
+//    }
+//}
