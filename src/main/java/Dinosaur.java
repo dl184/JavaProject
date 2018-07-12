f257b5b6448e2d52c9ecb5bc598a8e07847ba17d
Steven Bonner <stevenbonner1990@gmail.com>
2018-07-12 15:08:50 +0100

    updated dinosaur class

--- src/main/java/Dinosaur.java
+++ src/main/java/Dinosaur.java
@@ -1,2 +1,59 @@
 public class Dinosaur {
+
+    private int id;
+    private String type;
+    private String bloodType;
+    private int height;
+    private String color;
+
+    public Dinosaur() {
+    }
+
+    public Dinosaur(String type, String bloodType, int height, String color) {
+        this.type = type;
+        this.bloodType = bloodType;
+        this.height = height;
+        this.color = color;
+    }
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getBloodType() {
+        return bloodType;
+    }
+
+    public void setBloodType(String bloodType) {
+        this.bloodType = bloodType;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
 }
