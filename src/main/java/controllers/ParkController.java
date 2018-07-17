621a070207cdd9b19b6e5c0dd84e516e659a0343
Steven Bonner <stevenbonner1990@gmail.com>
2018-07-17 16:22:59 +0100

    fixed dino

--- src/main/java/controllers/ParkController.java
+++ src/main/java/controllers/ParkController.java
@@ -1,17 +1,17 @@
-//package controllers;
-
-import db.DBHelper;
-import models.*;
-import spark.ModelAndView;
-import spark.template.velocity.VelocityTemplateEngine;
-
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
-import static spark.Spark.get;
-import static spark.Spark.post;
-
+////package controllers;
+//
+//import db.DBHelper;
+//import models.*;
+//import spark.ModelAndView;
+//import spark.template.velocity.VelocityTemplateEngine;
+//
+//import java.util.HashMap;
+//import java.util.List;
+//import java.util.Map;
+//
+//import static spark.Spark.get;
+//import static spark.Spark.post;
+//
 //public class ParkController {
 //
 //
@@ -34,32 +34,32 @@ import static spark.Spark.post;
 //            return new ModelAndView(model, "templates/layout.vtl");
 //        }, new VelocityTemplateEngine());
 //
-//        get("/visitors", (req, res) -> {
+//        get("/park", (req, res) -> {
 //            Map<String, Object> model = new HashMap<>();
-//            List<Visitor> visitors = DBHelper.getAll(Visitor.class);
-//            model.put("template", "templates/visitors/index.vtl");
-//            model.put("visitors", visitors);
+//            List<Park> parks = DBHelper.getAll(Park.class);
+//            model.put("template", "templates/park/index.vtl");
+//            model.put("park", parks);
 //            return new ModelAndView(model, "templates/layout.vtl");
 //        }, new VelocityTemplateEngine());
 //
-//        get ("/visitors/new", (req, res) -> {
+//        get ("/park/new", (req, res) -> {
 //            Map<String, Object> model = new HashMap<>();
-//            List<Paddock> paddocks = DBHelper.getAll(Paddock.class);
-//            model.put("paddock", paddocks);
-//            model.put("template", "templates/visitors/create.vtl");
+//            List<Park> parks = DBHelper.getAll(Park.class);
+//            model.put("park", parks);
+//            model.put("template", "templates/park/create.vtl");
 //            return new ModelAndView(model, "templates/layout.vtl");
 //        }, new VelocityTemplateEngine());
 //
 //
-//        get("/visitors/:id", (req, res) -> {
+//        get("/park/:id", (req, res) -> {
 //            String strId = req.params(":id");
 //            Integer intId = Integer.parseInt(strId);
 //            Visitor visitor = DBHelper.find(intId, Visitor.class);
-//            List<Paddock> paddocks = (List<Paddock>) DBHelper.findVisitorInPark(visitor.getPark());
+//            List<Park> parks = (List<Park>) DBHelper.findVisitorInPark(visitor.getPark());
 //
 //            Map<String, Object> model = new HashMap<>();
 //
-//            model.put("visitor", visitor);
+//            model.put("park", park);
 //            model.put("paddock", paddocks);
 //            model.put("template", "templates/visitors/show.vtl");
 //
