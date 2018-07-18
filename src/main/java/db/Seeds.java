b1c7e093f44f4750c7d009523c7fc31ecef67287
Derek Leach <derekleach84@gmail.com>
2018-07-18 09:11:54 +0100

    added to seed files

--- src/main/java/db/Seeds.java
+++ src/main/java/db/Seeds.java
@@ -13,35 +13,62 @@ public class Seeds {
         DBHelper.deleteAll(Park.class);
         DBHelper.deleteAll(Visitor.class);
 
-        Paddock paddock1 = new Paddock("Trex Cell",4, DinosaurFood.PLANTS);
+        Paddock paddock1 = new Paddock("Tyrannosaurus Cell",4, DinosaurFood.PLANTS);
         DBHelper.saveOrUpdate(paddock1);
 
-        Paddock paddock2 = new Paddock("VelociraptorCell",5, DinosaurFood.GOAT);
+        Paddock paddock2 = new Paddock("Velociraptor Cell",5, DinosaurFood.GOAT);
         DBHelper.saveOrUpdate(paddock2);
 
-        Paddock paddock3 = new Paddock("BarneyCell", 20, DinosaurFood.COW);
+        Paddock paddock3 = new Paddock("Spinosaurus Cell", 3, DinosaurFood.COW);
         DBHelper.saveOrUpdate(paddock3);
 
-        Dinosaur dinosaur1 = new Dinosaur(paddock1, "Prehistoric Animals","Carnivores",609,"Green");
+        Paddock paddock4 = new Paddock("Triceratops Cell",6, DinosaurFood.GOAT);
+        DBHelper.saveOrUpdate(paddock4);
+
+        Paddock paddock5 = new Paddock("Stegosaurus Cell",2, DinosaurFood.COW);
+        DBHelper.saveOrUpdate(paddock5);
+
+        Paddock paddock6 = new Paddock("Brachiosaurus Cell",7, DinosaurFood.PLANTS);
+        DBHelper.saveOrUpdate(paddock6);
+
+        Paddock paddock7 = new Paddock("Allosaurus Cell",3, DinosaurFood.COW);
+        DBHelper.saveOrUpdate(paddock7);
+
+        Dinosaur dinosaur1 = new Dinosaur(paddock1, "Tyrannosaurus Rex","Carnivores",1500,"Green");
         DBHelper.saveOrUpdate(dinosaur1);
 
-        Dinosaur dinosaur2 = new Dinosaur(paddock2,"Raptors","Carnivores", 182,"Red");
+        Dinosaur dinosaur2 = new Dinosaur(paddock2,"Velociraptor","Carnivores", 600,"Red");
         DBHelper.saveOrUpdate(dinosaur2);
 
-        Dinosaur dinosaur3 = new Dinosaur( paddock3,"Tyrannosaurus rex","Herbivores", 152,"Purple");
+        Dinosaur dinosaur3 = new Dinosaur( paddock3,"Spinosaurus","Herbivores", 200,"Purple");
         dinosaur3.setHealthValue(40);
         DBHelper.saveOrUpdate(dinosaur3);
+
+        Dinosaur dinosaur4 = new Dinosaur(paddock1,"Triceratops","Carnivores", 900,"Red");
+        DBHelper.saveOrUpdate(dinosaur4);
+
+        Dinosaur dinosaur5 = new Dinosaur(paddock2,"Stegosaurus","Carnivores", 650,"Grey");
+        DBHelper.saveOrUpdate(dinosaur5);
+
+
+        Dinosaur dinosaur6 = new Dinosaur(paddock1,"Brachiosaurus","Carnivores", 300,"Green");
+        DBHelper.saveOrUpdate(dinosaur6);
+
+
+        Dinosaur dinosaur7 = new Dinosaur(paddock3,"Allosaurus","Carnivores", 500,"Red");
+        DBHelper.saveOrUpdate(dinosaur7);
+
 
         Park park = new Park("Dino World",5000,250);
         DBHelper.saveOrUpdate(park);
 
-        Visitor visitor1 = new Visitor("Jimmy Bob",100,25,182);
+        Visitor visitor1 = new Visitor("Steven Bonner",300,27,182);
         DBHelper.saveOrUpdate(visitor1);
 
-        Visitor visitor2 = new Visitor("Rick White", 50, 18,152);
+        Visitor visitor2 = new Visitor("Derek Leach", 500, 34,190);
         DBHelper.saveOrUpdate(visitor2);
 
-        Visitor visitor3 = new Visitor("IT",30000,32,190);
+        Visitor visitor3 = new Visitor("John Adams",3,56,167);
         DBHelper.saveOrUpdate(visitor3);
 
         DBPark.addDinosaurToPark(dinosaur1, park);
